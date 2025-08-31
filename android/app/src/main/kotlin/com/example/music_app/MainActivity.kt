9449bef37568d7e770e2b7612614b32dd3962eb1
Himanshu Yadav <himanshu40220@gmail.com>
2025-08-31 12:46:29 +0530

    fix issue of app killing on back button click

--- android/app/src/main/kotlin/com/example/music_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/music_app/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.example.music_app
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "app.minimizer"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "minimizeApp") {
+                moveTaskToBack(true) // ✅ sends app to background
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'pages/library_page.dart';
 import 'pages/music_player_page.dart';
 import 'package:flutter/services.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
@@ -35,6 +35,17 @@ class JustPLAY extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const channel = MethodChannel("app.minimizer");
+
+    // method to miniize app instead of killing on back button click
+    Future<void> minimizeApp() async {
+      try {
+        await channel.invokeMethod("minimizeApp");
+      } on PlatformException catch (e) {
+        print("Failed to minimize app: ${e.message}");
+      }
+    }
+
     final currentIndexProvider = Provider.of<CurrentIndexProvider>(context);
     final dataProvider = Provider.of<DataProvider>(context);
     dataProvider.loadSongsFromDB();
@@ -72,64 +83,72 @@ class JustPLAY extends StatelessWidget {
         ),
       ),
 
-      home: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: AlignmentGeometry.directional(0, 0.1),
-            colors: [
-              Colors.green.shade900, // Spotify green
-              Colors.black,
-            ],
+      home: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (didPop, result) async {
+          if (!didPop) {
+            // Send app to background (instead of killing)
+            await minimizeApp();
+          }
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: AlignmentGeometry.directional(0, 0.1),
+              colors: [
+                Colors.green.shade900, // Spotify green
+                Colors.black,
+              ],
+            ),
           ),
-        ),
-        child: Scaffold(
-          body: Column(
-            
-            children: [
-              Expanded(
-                child: pages[currentIndexProvider.navigationCurrentIndex],
-              ),
-              (dataProvider.clickedSong != null)
-                  ? Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: MiniPlayer(
-                        song: dataProvider.clickedSong!,
-                        currentIndexProvider: currentIndexProvider,
-                        dataProvider: dataProvider,
-                      ),
-                  )
-                  : SizedBox(),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            onTap: (index) {
-              if (index == 2) {
-                dataProvider.setClickedPlaylistToDownloads();
-              }
-              currentIndexProvider.setCurrentIndex(index);
-              currentIndexProvider.setNavigationIndex(index);
-            },
-            selectedItemColor: const Color(0xFF00D074),
-            unselectedItemColor: Colors.white70,
-            showUnselectedLabels: false,
-            showSelectedLabels: false,
-            iconSize: 30,
-            currentIndex: currentIndexProvider.currentIndex,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.library_music),
-                label: 'Library',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search, size: 35),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.cloud_download),
-                label: 'Downloads',
-              ),
-            ],
+          child: Scaffold(
+            body: Column(
+              children: [
+                Expanded(
+                  child: pages[currentIndexProvider.navigationCurrentIndex],
+                ),
+                (dataProvider.clickedSong != null)
+                    ? Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: MiniPlayer(
+                          song: dataProvider.clickedSong!,
+                          currentIndexProvider: currentIndexProvider,
+                          dataProvider: dataProvider,
+                        ),
+                      )
+                    : SizedBox(),
+              ],
+            ),
+            bottomNavigationBar: BottomNavigationBar(
+              onTap: (index) {
+                if (index == 2) {
+                  dataProvider.setClickedPlaylistToDownloads();
+                }
+                currentIndexProvider.setCurrentIndex(index);
+                currentIndexProvider.setNavigationIndex(index);
+              },
+              selectedItemColor: const Color(0xFF00D074),
+              unselectedItemColor: Colors.white70,
+              showUnselectedLabels: false,
+              showSelectedLabels: false,
+              iconSize: 30,
+              currentIndex: currentIndexProvider.currentIndex,
+              items: const [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.library_music),
+                  label: 'Library',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search, size: 35),
+                  label: 'Search',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.cloud_download),
+                  label: 'Downloads',
+                ),
+              ],
+            ),
           ),
         ),
       ),
